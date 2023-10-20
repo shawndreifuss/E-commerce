5309daf6e85ea41d25b10419ece2f45d503848a8
Shawn Dreifuss <shawndreifuss@icloud.com>
2023-10-19 17:17:39 -0700

    finished delete request for category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -37,13 +37,51 @@ router.post('/', async (req, res) => {
     res.status(500).json(err);
   };
 });
+// update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update({
+      category_name: req.body.categoryName
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    });
+    // sends message if user requests to update ID that does not exist
+    const categoryById = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+    if (!categoryById) {
+      res.status(200).json({message: 'No category found'});
+      return;
+    }
+    res.status(200).json(categoryData);
+    console.log("Category updated!");
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const categorydata = await Category.destroy(
+      {
+        where: {
+          id: req.params.id,
+        }
+      });
+      if (!categorydata) {
+        res.status(200).json({message: 'No categories found'});
+        return;
+      };
+      res.status(200).json(categorydata);
+      console.log('Category deleted!');
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 module.exports = router;
