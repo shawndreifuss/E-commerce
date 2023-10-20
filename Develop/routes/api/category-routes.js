d99db099ce1a328cfebe8a5f7e29e8f71fd1046d
Shawn Dreifuss <shawndreifuss@icloud.com>
2023-10-19 17:06:30 -0700

    added route for /categories

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -2,10 +2,16 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// find all categories be sure to include its associated Products
+router.get('/', async (req, res) => {
+   try {
+    const categoryData = await Category.findAll({
+      include: [{model: Product}]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
