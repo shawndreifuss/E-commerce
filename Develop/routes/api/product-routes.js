bddfb7fe1b2387b67e8bcdc9e042834d5eda4eb9
Shawn Dreifuss <shawndreifuss@icloud.com>
2023-10-19 17:20:48 -0700

    added product route

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,15 +4,35 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
+    if (!productData) {
+      res.status(200).json({message: 'No products found'});
+      return;
+    }
+    res.json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+// find a single product by its `id`, include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }]
+    });
+    if (!productData) {
+      res.status(200).json({message: 'No products found'});
+      return
+    }
+    res.status(200).json(productdata);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
